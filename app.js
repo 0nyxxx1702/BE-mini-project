fd4c12cc920edaedcf336b6deee8843ab845a8db
Swastik Ranjan Panigrahi <swastikrpanigrahi@gmail.com>
2024-09-17 09:23:35 +0530

    added postSchema, bcrypt, jwt and registration

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const app = express();
 const userSchema=require('./models/user');
-const cookieParser=require('cookie-parser')
+const postSchema=require('./models/post');
+const cookieParser=require('cookie-parser');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -10,6 +13,28 @@ app.set("view engine", "ejs");
 
 app.get('/', (req,res)=>{
     res.render("index");
+});
+app.post('/register', (req,res)=>{
+    let {name, username, password, email, age}=req.body;
+    let user=userModel.findOne({email});
+    if(user) return res.status(500).send("User already registered");
+
+    bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(password, salt, async (err, hash)=>{
+            let user=await userModel.create({
+                username,
+                age, 
+                name,
+                email,
+                password: hash
+            });
+
+            let token=jwt.sign({email: emaik, userid: user._id}, "shhhh");
+            res.cookie("token", token);
+            res.send("registered");
+        })
+    })
+
 });
 
 app.listen(3000);
